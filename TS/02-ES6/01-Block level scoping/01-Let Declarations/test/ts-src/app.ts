e480f72e0f64c8e9f3b9d29baf60754a55f4d59d
jagchat <jag.chat@gmail.com>
2016-11-30 15:59:28 -0600

    ES6 samples added

---
+++ TS/02-ES6/01-Block level scoping/01-Let Declarations/test/ts-src/app.ts
@@ -0,0 +1,35 @@
+declare var $:any;
+
+function doProcess1(p){
+	//does not throw error even if 'v' is not declared earlier
+	//displays undefined (hoisted first, assigned later)
+	console.log(v); 
+	//console.log(v2); //throws compilation error
+	if(p == 1){
+		var v = 10;		
+	}
+	else{
+		var v = 20;
+	}
+	return v; //works, no error as 'v' is hoisted
+}
+
+
+function doProcess2(p){
+	//console.log(v); //throws compilation error
+	//console.log(v2); //throws compilation error
+	if(p == 1){
+		let v = 10; //available only to this block
+		return v;
+	}
+	else{
+		let v = 20; //available only to this block
+		return v;
+	}
+	//return v; //throws compilation error
+}
+
+$(function(){
+	//console.log(doProcess1(2));
+	console.log(doProcess2(2));
+});
