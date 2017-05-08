65d714e4b0cdcf6163ea5952c782dbb4511b5be6
jagchat <jag.chat@gmail.com>
2017-05-08 10:30:54 -0500

    new samples added

---
+++ angular-2/02-Setup using VS2017/WebApplication1/WebApplication1/gulpfile.js
@@ -0,0 +1,22 @@
+﻿var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var tsProject = ts.createProject("tsconfig.json");
+var sourcemaps = require('gulp-sourcemaps');
+
+//gulp.task("default", function () {
+//    return tsProject.src()
+//        .pipe(tsProject())
+//        .js.pipe(gulp.dest("."));
+//});
+
+gulp.task("default", function () {
+    return tsProject.src()
+        .pipe(sourcemaps.init())
+        .pipe(tsProject()).js
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("app"));
+});
+
+gulp.task('dev', function () {
+    gulp.watch('app/**/*.ts', ['default']);
+});
