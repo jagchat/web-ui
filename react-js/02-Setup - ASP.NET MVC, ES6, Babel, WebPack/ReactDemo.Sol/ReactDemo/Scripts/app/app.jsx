0979eb1a134ab87c793af8102105cf21084f229b
jagchat <jag.chat@gmail.com>
2017-09-07 16:32:02 -0500

    react sample added

---
+++ react-js/02-Setup - ASP.NET MVC, ES6, Babel, WebPack/ReactDemo.Sol/ReactDemo/Scripts/app/app.jsx
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        let s = "Hello World!";
+        return <div>{s}</div>;
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app-container'));
---
+++ react-js/02-Setup - ASP.NET MVC, ES6, Babel, WebPack/ReactDemo.Sol/ReactDemo/webpack.config.js
@@ -0,0 +1,26 @@
+﻿var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: ['./Scripts/app/app.jsx'],
+    output: {
+        path: './build',
+        filename: 'app.js',
+        sourceMapFilename: 'app.js.map'
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx$/,
+            include: path.resolve(__dirname, "Scripts/app"),
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    },
+    resolve: {
+        extensions: ["", ".webpack.js", ".web.js", ".jsx", ".js"]
+    },
+    watch: false
+};
