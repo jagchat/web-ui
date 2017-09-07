63a61ed8608321dbda30cf5f2a2c10be2b52d20d
jagchat <jag.chat@gmail.com>
2017-09-07 16:58:32 -0500

    new sample added

---
+++ react-js/03-Setup - ASP.NET MVC, ES6, Babel, Browserify, Babelify, Grunt/ReactDemo.Sol/ReactDemo/gruntfile.js
@@ -0,0 +1,33 @@
+﻿module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        watch: {
+            browserify: {
+                files: ['scripts/**/*.jsx'],
+                tasks: ['browserify']
+            }
+        },
+
+        browserify: {
+            dev: {
+                options: {
+                    transform: [['babelify', { presets: ['es2015', 'react'] }]],
+                    browserifyOptions: {
+                        debug: true
+                    }
+                },
+                src: ['scripts/**/*.jsx'],
+                dest: 'build/app.js'
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browserify');
+
+    grunt.registerTask('default', ['browserify']);
+
+};
