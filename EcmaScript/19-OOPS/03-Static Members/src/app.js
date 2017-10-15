2c2157c75e4d7b85138e4ccc25a4573bba444fac
jagchat <jag.chat@gmail.com>
2017-10-15 10:44:26 -0500

    ES sample added

---
+++ EcmaScript/19-OOPS/03-Static Members/src/app.js
@@ -0,0 +1,33 @@
+import $ from 'jquery'
+
+// Iterable class
+class Sample {
+
+	constructor() {
+		this.a = 0;
+		this.b = 0;
+	}
+
+	//method
+	sum() {
+		return this.a + this.b;
+	}
+
+	//static method
+	static createSampleObject(m = 0, n = 0) {
+		let o = new Sample();
+		o.a = m;
+		o.b = n;
+		return o;
+	}
+}
+
+$(function () {
+
+	let o1 = Sample.createSampleObject();
+	console.log(`Sum = ${o1.sum()}`);
+
+	let o2 = Sample.createSampleObject(5, 6);
+	console.log(`Sum = ${o2.sum()}`);
+
+});
