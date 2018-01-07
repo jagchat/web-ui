9e8c9863da5585b08c836fb98d0fb2d6050cf97a
jagchat <jag.chat@gmail.com>
2018-01-06 22:53:36 -0600

    ng bootstrap sample added

---
+++ angular-2/04-Components/04-Including Bootstrap using NPM/test/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+//All bootstrap artifacts are added in index.htm
+@Component({
+    selector: 'app',
+    moduleId: module.id,
+    templateUrl: 'app.component.html',
+
+})
+export class AppComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { }
+}
