8ee8f1d9c24eaef4107ea68a46f46742c57c7502
jagchat <jag.chat@gmail.com>
2018-01-06 23:42:31 -0600

    ng-bootstrap sample added

---
+++ angular-2/04-Components/05-Including Bootstrap using ng-bootstrap/test/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+//bootstrap configured in systemjs.config.js, index.htm
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+    imports: [BrowserModule, NgbModule],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
