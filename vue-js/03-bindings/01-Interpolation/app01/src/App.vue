61d01cb3819a3006c424c4ffaa5e84d7f8229340
jagchat <jag.chat@gmail.com>
2018-10-05 13:15:37 -0500

    vue samples updated

---
+++ vue-js/03-bindings/01-Interpolation/app01/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <h1>{{ normalMsg }}</h1>
+    <h1>{{ htmlMsg }}</h1>
+    <h1 v-html="htmlMsg"></h1>
+
+    <div v-bind:id="someId">attribute binding for id</div>
+    <div v-bind:id="'a' + someId">attribute binding and concatenation</div>
+    <div v-bind:id="someId+1">attribute binding and js expression</div>
+    <div>Interpolation and Js Expression: {{ someId + 1 }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      normalMsg: "Normal Text (interpolation)",
+      htmlMsg: "<strong>Html Text</strong>",
+      someId: 1
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+</style>
