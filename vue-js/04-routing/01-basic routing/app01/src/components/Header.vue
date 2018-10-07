4da60896984bafd7b8dfad1c19ea6349869a310c
jagchat <jag.chat@gmail.com>
2018-10-07 11:39:30 -0500

    vue samples updated

--- vue-js/04-routing/01-basic routing/app01/src/components/Header.vue
+++ vue-js/04-routing/01-basic routing/app01/src/components/Header.vue
@@ -3,6 +3,9 @@
         <h1>Header</h1>
         <div><router-link to="/Home">Home Page</router-link></div>
         <div><router-link to="/About">About Page</router-link></div>
+        or
+        <router-link to="/Home" tag="div" active-class="active-part">Home Page</router-link>
+        <router-link to="/About" tag="div" active-class="active-part">About Page</router-link>
         <hr>
     </div>
 </template>
@@ -14,3 +17,9 @@ export default {
   }
 };
 </script>
+<style>
+.router-link-active,
+.active-part {
+  background-color: cyan;
+}
+</style>
